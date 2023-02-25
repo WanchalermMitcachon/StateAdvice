36260e010d7865ba6d1f995ec59d463a76222d58
Wanchalerm Mitcachon <116241970+WanchalermMitcachon@users.noreply.github.com>
2023-02-25 13:45:16 +0700

    แก้ JS
    แก้ JS

--- advice-generator-app-main/script.js
+++ advice-generator-app-main/script.js
@@ -2,6 +2,8 @@ const diceBtn=document.getElementById('dice')
 const idRandom=document.getElementById('random-id')
 const textMain=document.querySelector('.text-main')
 
+
+
   async function showData(){
     try{
         const response=await fetch('https://api.adviceslip.com/advice');
@@ -15,8 +17,13 @@ const textMain=document.querySelector('.text-main')
     }
 }
 diceBtn.addEventListener('click',()=>{
-   
+   loading()
     setTimeout(showData,1500)
    
 })
+
+function loading(){
+    idRandom.innerText=`Loading`
+    textMain.innerHTML=`Loading`
+}
 
