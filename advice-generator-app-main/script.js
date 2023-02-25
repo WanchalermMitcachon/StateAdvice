dc2593ea933afea9393e1e261670b5bcd1d3ba0a
Wanchalerm Mitcachon <116241970+WanchalermMitcachon@users.noreply.github.com>
2023-02-25 13:22:32 +0700

    Add files via upload

---
+++ advice-generator-app-main/script.js
@@ -0,0 +1,22 @@
+const diceBtn=document.getElementById('dice')
+const idRandom=document.getElementById('random-id')
+const textMain=document.querySelector('.text-main')
+
+  async function showData(){
+    try{
+        const response=await fetch('https://api.adviceslip.com/advice');
+        const data=await response.json();
+        console.log(data.slip.id)
+        idRandom.innerHTML=`${data.slip.id}`
+        textMain.innerText=`${data.slip.advice}`
+
+        }catch(error){
+        console.log(error)
+    }
+}
+diceBtn.addEventListener('click',()=>{
+   
+    setTimeout(showData,1500)
+   
+})
+
