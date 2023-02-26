53c48ca29ec6de8473fec116368c02bff6619a9d
Wanchalerm Mitcachon <116241970+WanchalermMitcachon@users.noreply.github.com>
2023-02-26 20:49:43 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+const diceBtn=document.getElementById('dice')
+const idRandom=document.getElementById('random-id')
+const textMain=document.querySelector('.text-main')
+
+showData()
+
+  async function showData(){
+    try{
+        const response=await fetch('https://api.adviceslip.com/advice');
+        const data=await response.json();
+        console.log(data.slip.id)
+        idRandom.innerHTML=`${data.slip.id}`
+        textMain.innerText=`${data.slip.advice}`
+
+        }catch(error){
+        console.log(error)
+    }
+}
+diceBtn.addEventListener('click',()=>{
+   
+    loading()
+    setTimeout(showData,1500)
+   
+})
+
+function loading(){
+    idRandom.innerText=`Loading`
+    textMain.innerHTML=`Loading`
+}
+
